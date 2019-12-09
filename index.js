f1d98ba0bdf7fbb49a4d1507b6879c246c5a53f5
Ian Brito <ianbazevedo@gmail.com>
2019-12-09 01:52:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const fs = require("fs");
 const parser = require("./parser");
 
 if (readline.keyInYN("Deseja criar um arquivo js da gramática gerada")) {
-  fs.writeFileSync("output.js", parser.generate(), err => {
-    //Caro ocorra algum erro
+  fs.writeFileSync(`export_${Date.now()}.js`, parser.generate(), err => {
     if (err) {
       return console.log("erro");
     }
-    //Caso não tenha erro, retornaremos a mensagem de sucesso
     console.log("Arquivo Criado");
   });
 }
 console.log(`
   Operações aceita: adição, subtração, multiplicação e divisão.
-`)
+`);
 const expressao = readline.question("Digite a expressão aritmética: ");
 let resultado = parser.parse(expressao);
 
